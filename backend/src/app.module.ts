c9c4bf321b877c9bcb79e44f1f1a20e349dc19ad
youssef1129 <maazouzyoussef11@gmail.com>
2026-08-05 11:43:34 +0200

    pg/typeorm connect

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
 })
 export class AppModule {}
---
+++ backend/src/database/database.providers.ts
@@ -0,0 +1,22 @@
+import { DataSource } from 'typeorm';
+import { DATA_SOURCE } from '../common/constants';
+
+export const databaseProviders = [
+  {
+    provide: DATA_SOURCE,
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: process.env.DB_HOST ?? 'localhost',
+        port: parseInt(process.env.DB_PORT!) ?? 21503,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
