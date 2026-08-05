05eb8616393b12592498d7a992a942a4b776b3a3
youssef1129 <maazouzyoussef11@gmail.com>
2026-08-05 10:42:14 +0200

    add dockerfile, docker compose and swagger config

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
-@Controller()
+@Controller('')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Portail API';
+  }
+
+  @Get('health')
+  getHealth(): { status: string } {
+    return { status: 'OK' };
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+  app.setGlobalPrefix('api/v1', {
+    exclude: ['health'],
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Portail API')
+    .setDescription('API pour le portail des avocats')
+    .setVersion('1.0')
+    .addTag('portail')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 21501);
 }
 bootstrap();
